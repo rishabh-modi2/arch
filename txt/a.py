a88b0cced75338ccbeab8b0e7f7099768bd948d2
rishabh-modi2 <modirishabh0322@gmail.com>
2022-05-28 07:44:28 +0000

    index

--- txt/a.py
+++ txt/a.py
@@ -1,4 +1,3 @@
 import os
 for a in os.listdir('./'):
-    a1 = a.replace('.txt', '.html')
-    os.system("mv " + a + ' ' + a1)
+    print("<li><a href='" + a + "'><span style='font-family: 'andale mono', monospace;'><strong>" + a + "</strong></span> </a></li>")
