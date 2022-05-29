665b95f7c4d31d26c16303fce2c98d7ef9122a65
rishabh-modi2 <modirishabh0322@gmail.com>
2022-05-29 08:31:35 +0000

    n

--- txt/Video/a.py
+++ txt/Video/a.py
@@ -1,10 +1,10 @@
 import os
-file = '../saiman/'
-os.system('rm ' + file + 'index.html')
-with open(file + 'index.html', 'a+') as f:
-    f.write("        <style>\n            li {\n  color: white;\n  font-family: sans-serif;\n  font-size:5vh;\n}\n        </style>\n<p><span style='font-size: 14pt; color: black;'><strong>Made By u/Rishabhmoodi</strong></span></p>")        
+file = '../../'
+os.system('rm ' + file + 'index2.html')
+with open(file + 'index2.html', 'a+') as f:
+    f.write("        <style>\n            li {\n  color: white;\n  font-family: sans-serif;\n  font-size:3vh;\n}\n        </style>\n<p><span style='font-size: 14pt; color: black;'><strong>Made By u/Rishabhmoodi</strong></span></p>")        
 for o in os.listdir(file):
     print(o)
-    with open(file + 'index.html', 'a+') as f:
-        f.write("<li><a href='" + o + "'><span style='font-family: 'andale mono', monospace;'><strong> + o + </strong></span> </a></li>")        
+    with open(file + 'index2.html', 'a+') as f:
+        f.write("<li><a href='" + o + "'><span style='font-family: 'andale mono', monospace;'><strong>" + o + "</strong></span> </a></li>")        
     
