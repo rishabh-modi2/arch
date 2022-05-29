08fbc3ae0a96075194f4afa630b4b6c134c350bf
rishabh-modi2 <modirishabh0322@gmail.com>
2022-05-29 16:23:07 +0000

    h

--- txt/Video/a.py
+++ txt/Video/a.py
@@ -7,5 +7,5 @@ for o in os.listdir(file):
     print(o)
     o2 = o.replace('.html', '.txt')
     with open(file + 'index.html', 'a+') as f:
-        f.write("<a href=" + o +">" + o2 + "</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n")        
+        f.write("<a href=/" + o +">" + o2 + "</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n")        
     
