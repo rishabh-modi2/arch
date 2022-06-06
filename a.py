c7d81cc321f047ddea53253cd6e4478efaef4245
rishabh-modi2 <modirishabh0322@gmail.com>
2022-06-06 09:16:51 +0000

    a

---
+++ a.py
@@ -0,0 +1,11 @@
+import os
+file = input('input')
+os.system('rm ' + file + '/index.html')
+with open(file + '/' + 'index.html', 'a+') as f:
+    f.write("<head>\n         <link rel='stylesheet' href='https://collection.rishabh.ml/indexstyle.css'>\n</head>\n<p><span'><strong>Made By u/Rishabhmoodi</strong></span></p>")        
+for o in os.listdir(file):
+    print(o)
+    o2 = o.replace('.html', '.txt')
+    with open(file + '/' + 'index.html', 'a+') as f:
+        f.write("<a href=/" + o +">" + o2 + "</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n")        
+    
