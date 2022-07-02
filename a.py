4409db91616cf19ab797e675ee185edee9e87214
rishabh-modi2 <modirishabh0322@gmail.com>
2022-07-02 16:12:47 +0000

    img+perma

--- a.py
+++ a.py
@@ -1,13 +1,13 @@
 import os
 import natsort
 #file = input('input')
-file = './txt/raw/'
+file = './Chodi/Text'
 os.system('rm ' + file + '/index.html')
 with open(file + '/' + 'index.html', 'a+') as f:
     f.write("<head>\n         <link rel='stylesheet' href='https://collection.rishabh.gq/indexstyle.css'>\n</head>\n<p><span'><strong>Made By u/Rishabhmoodi</strong></span></p>")        
 for o in natsort.natsorted(os.listdir(file)):
     print(o)
-    o2 = o.replace('.html', '.txt')
+    o2 = o.replace('.html', '')
     with open(file + '/' + 'index.html', 'a+') as f:
         f.write("<a href=" + o +">" + o2 + "</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\n")        
     
